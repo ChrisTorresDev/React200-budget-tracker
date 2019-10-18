a581b16ec95b6ae26580b276136cc4b40579a6ac
Chris Torres <hwct83@gmail.com>
2019-10-17 17:18:48 -0700

    set up Redux Store

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,4 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './app';
 
-render(<App />, document.getElementById('root'));
+render(
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
